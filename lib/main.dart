c7a8c7d213e5f4c0d3660670ccccfbf89934df2b
Alefiya Rajgarh <alefiyarajgarh@gmail.com>
2025-08-08 16:37:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:chatbot/chat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ChatScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/message.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+class Message {
+  // Indicates whether the message was sent by the user (true) or received (false).
+  final bool isSender;
+
+  // The content of the message as a string.
+  final String msg;
+  final DateTime timestamp;
+  final File? imageFile;
+
+  // Constructor to initialize the Message object with the sender status and message content.
+  Message(this.isSender, this.msg, this.timestamp, {this.imageFile});
+}
